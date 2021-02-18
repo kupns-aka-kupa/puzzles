90e52d27ea0d205451d0a49b687248174c3356fe
kupns <kupns@mail.ru>
2021-02-18 23:48:51 +0300

    add table

---
+++ mainwindow.cpp
@@ -0,0 +1,52 @@
+#include "mainwindow.hpp"
+#include "./static/ui/ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , model(new QStandardItemModel(5, 5))
+    , table(new Table{5, 5})
+    , delegate(new TableViewItemDelegate(this))
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    auto tableView = ui->tableView;
+    tableView->setModel(model);
+    tableView->setEditTriggers(QAbstractItemView::NoEditTriggers);
+
+    QList<QHeaderView*> headers
+    {
+        tableView->horizontalHeader(),
+        tableView->verticalHeader()
+    };
+
+    Q_FOREACH(auto header, headers)
+    {
+        header->setSectionResizeMode(QHeaderView::Stretch);
+        header->hide();
+    }
+
+    QFont cellFont("mainfont.ttf", 24, QFont::Normal);
+    tableView->setFont(cellFont);
+
+    for(int i = 0; i < model->columnCount(); i++)
+    {
+        for(int j = 0; j < model->rowCount(); j++)
+        {
+            QModelIndex index = model->index(i, j, QModelIndex());
+            model->setData(index, i + j);
+        }
+    }
+}
+
+void loadFont()
+{
+    QFile file(":/fonts/static/fonts/mainfont.ttf");
+    qDebug() << file.exists();
+    QFontDatabase::addApplicationFont(file.fileName());
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+    delete table;
+}
---
+++ mainwindow.hpp
@@ -0,0 +1,32 @@
+#ifndef MAINWINDOW_HPP
+#define MAINWINDOW_HPP
+
+#include <QMainWindow>
+#include <QFile>
+#include <QList>
+#include <QDebug>
+#include <QAbstractItemView>
+#include <QFontDatabase>
+#include <QStandardItemModel>
+#include "tableviewitemdelegate.hpp"
+#include "table.hpp"
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+private:
+    QStandardItemModel *model;
+    Table *table;
+    TableViewItemDelegate *delegate;
+    Ui::MainWindow *ui;
+};
+#endif // MAINWINDOW_HPP
